2134921ff9ae5098975c52ce4380b33c710f7184
margaZM <aurazambrano227@gmail.com>
2021-10-12 20:58:24 -0500

    Agrega funcion para registrar usuario

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,165 @@
-import React from 'react'
+// import { useState } from 'react';
+import { UserOutlined, MailOutlined, KeyOutlined, CheckOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import {
+    Form,
+    Input,
+    Select,
+    Button,
+    notification
+} from 'antd';
+import { registerUser } from '../firebase/firebase'
+import { auth } from '../firebase/firebaseConfig'
 
 function Register() {
+    const [form] = Form.useForm();
+    const { Option } = Select;
+
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+        registerUser(auth, values.email, values.password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user)
+            notification.open({
+                icon: <CheckOutlined style={{ color: '#00FF00' }}/>,
+                description:
+                  'Registrado exitosamente',
+                onClick: () => {
+                  console.log('Notification Clicked!');
+                },
+            });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage)
+        });
+    };
+
+    const formFailed = (error) => {
+    console.log('Error:', error);
+    }
+
     return (
-        <div>
-            <h1>Bienvenida al Register</h1>
-            
+
+        <div className="ant-row form-container">
+            <div className="img-burger ant-col ant-col-sm-0 ant-col-xl-10"> </div>
+            <div className="ant-col-xl-3"> </div>
+            <Form
+                layout='vertical'
+                className="ant-col ant-col-sm-24 ant-col-xl-11 form-register"
+                autoComplete="off"
+                form={form}
+                name="register"
+                onFinish={onFinish}
+                onFinishFailed={formFailed} 
+                scrollToFirstError
+                >
+
+                <Form.Item
+                    label="Nombre de usuario"
+                    name="username"
+                    rules={[
+                        { required: true, 
+                            message: 'Por favor, introduce tu nombre de usuario' 
+                        }
+                    ]}
+                    >
+                    <Input
+                        placeholder="Introduce tu nombre completo"
+                        prefix={ <UserOutlined/> }
+                    />
+                </Form.Item>
+
+
+                <Form.Item
+                    name="email"
+                    label="Correo Electrónico"
+                    rules={[
+                    {
+                        type: 'email',
+                        message: 'Correo Electrónico inválido',
+                    },
+                    {
+                        required: true,
+                        message: 'Por favor ingrese su Correo Electrónico!',
+                    },
+                    ]}
+                    >
+                    <Input 
+                    placeholder="Introduce tu email"
+                    prefix={ <MailOutlined/> }
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="password"
+                    label="Password"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Por favor ingrese su contraseña',
+                    },
+                    ]}
+                    hasFeedback
+                    >
+                    <Input.Password
+                        placeholder="Introduce tu contraseña"
+                        prefix={ <KeyOutlined/> }
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    label="Confirmar Contraseña"
+                    dependencies={['password']}
+                    hasFeedback
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Please confirm your password!',
+                    },
+                    ({ getFieldValue }) => ({
+                        validator(_, value) {
+                        if (!value || getFieldValue('password') === value) {
+                            return Promise.resolve();
+                        }
+
+                        return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                        },
+                    }),
+                    ]}
+                    >
+                    <Input.Password 
+                        placeholder="Confirma tu contraseña"
+                        prefix={ <KeyOutlined/> }
+                    />
+                </Form.Item>
+
+                <Form.Item
+                    name="job"
+                    label="Cargo"
+                    rules={[
+                    {
+                        required: true,
+                        message: 'Por favor seleccione su cargo',
+                    },
+                    ]}
+                    >
+                    <Select placeholder="Seleccione su cargo...">
+                    <Option value="mesero">Mesero</Option>
+                    <Option value="cocina">Jefe de cocina</Option>
+                    </Select>
+                </Form.Item>
+
+                <Form.Item >
+                    <Button htmlType="submit" className="register-form-button">
+                    REGISTRARSE
+                    </Button>
+                </Form.Item>
+            </Form>            
         </div>
     )
 }
