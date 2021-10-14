53eb8c79b301c7321987d7e15fdea510604a854c
Andrea Blanco <estefania_8_3@hotmail.com>
2021-10-13 22:36:48 -0500

    feat: added nav boilerplate

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,17 +9,36 @@ import {
   DesktopOutlined,
   ContainerOutlined,
   MailOutlined,
+  LoginOutlined
 } from '@ant-design/icons';
 
-const { SubMenu } = Menu;
+const { Item } = Menu;
 
 
 function Nav() {
   return (
     <div>
-      <Button>
+      <Button type="primary">
         <MenuUnfoldOutlined />
       </Button>
+      <Menu>
+        <Item key="profile" icon={<img src="https://img.icons8.com/small/16/000000/user-male-circle.png"/>}>Profile</Item>
+      </Menu>
+      <Menu>
+        <Item key="menu" icon={<img src="https://img.icons8.com/small/16/000000/restaurant-menu.png"/>}>Menu</Item>
+      </Menu>
+      <Menu>
+        <Item key="preparing" icon={<img src="https://img.icons8.com/small/16/000000/time.png"/>}>En preparación</Item>
+      </Menu>
+      <Menu>
+        <Item key="done" icon={<img src="https://img.icons8.com/small/16/000000/service-bell.png"/>}>Pedidos listos</Item>
+      </Menu>
+      <Menu>
+        <Item key="delivered" icon={<img src="https://img.icons8.com/small/16/000000/checkmark.png"/>}>Despachados</Item>
+      </Menu>
+      <Menu>
+        <Item key="logout" icon={<img src="https://img.icons8.com/small/16/000000/logout-rounded.png"/>}>Cerrar Sesión</Item>
+      </Menu>
     </div>
   )
 }
