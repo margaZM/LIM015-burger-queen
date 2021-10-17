a76d39063517c4845ade20984a0b92d41a618e3c
Andrea Blanco <estefania_8_3@hotmail.com>
2021-10-17 02:25:07 -0500

    feat: added error404 view

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,37 @@
 import 'antd/dist/antd.css';
-import { Result, Button } from 'antd';
+import { Row, Col, Image, Tag, Space } from 'antd';
+import '../css/Error404.css';
+import error404 from '../images/error404.svg';
 
 function Error404() {
-  return(
-    <h1>HOLA MUNDO!</h1>
+  return (
+    <section className="error-container">
+      <Row gutter={[16, 16]}>
+        <Col md={24} lg={24}>
+          <p className="text-error">Oops! Lo sentimos pero no se encontro lo que buscabas</p>
+          <figure>
+            <Image preview={false} src={error404} alt={"not found"} />
+          </figure>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]}>
+        <Col md={24} lg={24}>
+          <p className="text-error">O tal vez quisiste visitar alguno de los sgtes links:</p>
+        </Col>
+      </Row>
+      <Row gutter={[16, 16]}>
+        <Col>
+          <Tag>
+            <a href="/">Inicio</a>
+          </Tag>
+        </Col>
+        <Col>
+          <Tag>
+            <a href="/register">Registro</a>
+          </Tag>
+        </Col>
+      </Row>
+    </section>
   );
 }
 
