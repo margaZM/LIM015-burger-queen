d2c9504164589f058bf7b3c2ab31db4f52585849
margaZM <aurazambrano227@gmail.com>
2021-10-26 09:15:29 -0500

    ADD: Create funcionality to add and subtract a product and add to the firestore

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -2,18 +2,18 @@ import { useState, useEffect } from 'react'
 import { db } from '../firebase/firebaseConfig';
 import { Layout, Row, Col, Tabs } from 'antd';
 import '../css/main.css'; 
+import swal from 'sweetalert';
 import Nav from '../components/nav.jsx';
 import OrderForm from '../components/OrderForm.jsx';
 import MenuCards from '../components/MenuCards.jsx';
 import { querySnapshot } from '../firebase/firestore';
-import { CustomerServiceOutlined } from '@ant-design/icons';
+// import { CustomerServiceOutlined } from '@ant-design/icons';
 
 const { Content, Header } = Layout; 
 const { TabPane } = Tabs; 
 
 function MenuApp() {
-
-  //-------------Traer data de firebase para renderizar vista mesero ------------//
+  //---------------Traer data de firebase para renderizar vista mesero     ----------------------//
   const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
@@ -25,62 +25,55 @@ function MenuApp() {
     getProducts().then((productsArray) => setProducts(productsArray))
   }, []);
 
-
-  //------Traer cada producto seleccionado para agregar en resumen de pedido ------//
-  const [selectedProductsArray, setSelectedProductsArray] = useState([])
-
-  const selectedProduct = (product) => {
-    console.log(product)
-    // Hacemos un nuevo objeto con el producto seleccionado
+  //---------------------------Agregar un producto en resumen de pedido --------------------------//
+  const [selectedProductsArray, setSelectedProductsArray] = useState([]);
+  const handleAddProduct = (product) => {
     const newProduct = {
       id: product.id,
       name: product.docdata.name,
       price: product.docdata.price,
-      total: product.docdata.price,
+      type: product.type,
       quantity: 1
     }
-    //1.- Verificamos si existe ya en el array 
+
     const isProductInTheArray = selectedProductsArray.some(product => product.id === newProduct.id)
-    console.log(isProductInTheArray)
-    // 2.1 Si ya existe en el array actualizo la cantidad y precio total
     if ( isProductInTheArray ) { 
-      const products = selectedProductsArray.map(product => {// Iteramos en el array original y verificamos si hay duplicados
-        if ( product.id === newProduct.id ) { //Si el id del producto en el array es igual al que estoy intentando agregar lo actualizo
-          product.quantity++;
-          product.total = product.price * product.quantity;
-          return product; //Retorna producto actualizado
-        }
-        else { //
-          return product; //Retorna producto sin actualizar porque no existe
-        }
-      })
+      const products = selectedProductsArray.map(product => // Iteramos en el array original y verificamos si hay duplicados
+        product.id === newProduct.id ? { ...product, quantity: product.quantity + 1} : product
+      )
       setSelectedProductsArray([...products])
     } else {
-      //2.2 Agrego productos al array
       setSelectedProductsArray([...selectedProductsArray, newProduct])
     }
-  
-    // setCartItems((prev) => {
-    //   // Search the item in the array
-    //   const isItemInTheCart = prev.find((i) => i.id === item.id);
-    //   if (isItemInTheCart) {
-    //     return prev.map((i) =>
-    //       i.id === item.id ? { ...i, amount: i.amount + 1 } : i
-    //     );
-    //   }
-    //   return [...prev, { ...item, amount: 1 }];
-    // });
+  }
 
+  //------------------------   Eliminar un producto del resumen del pedido -------------------------//
+  const handleDeleteProduct = (product) => {
+    swal({
+      text: '⚡ ¿Está seguro que desea eliminar este producto? ⚡',
+      icon: 'warning',
+      buttons: ['No', 'Si']
+    }).then(res => {
+      if (res) {
+        setSelectedProductsArray((selectedProductsArray) =>
+        selectedProductsArray = selectedProductsArray.filter(item => item.id !== product.id ) //filtramos para que actualice el estado excluyendo este producto
+      )
+        swal({text: 'Producto eliminado con éxito', icon: 'success',})
+      }
+    })
+  }
 
-    // const act = selectedProductsArray.map(product => 
-    //   console.log(product),
-    //   product.id === newProduct.id ? 
-    //   product.quantity++ && (product.price *= product.quantity)
-    //   : product
-    // )
+  //-----------------------Restar un producto del resumen del pedido ---------------------------/
+  const handleMinusProduct = (product) => {
+    product.quantity === 1 ? handleDeleteProduct(product) : 
+    selectedProductsArray.map(item => item.id === product.id ? item.quantity-- :  item)  
+    setSelectedProductsArray([...selectedProductsArray]);
+  }
 
-    // setSelectedProductsArray([...selectedProductsArray, newProduct])
-    // console.log(selectedProductsArray)
+  //-----------------------Agregar un producto en el resumen del pedido ---------------------------/
+  const handlePlusProduct = (product) => {
+    selectedProductsArray.map(item => item.id === product.id ? item.quantity++ :  item) //filtramos para que actualice el estado excluyendo este producto
+    setSelectedProductsArray([...selectedProductsArray]);
   }
 
   return (
@@ -106,15 +99,8 @@ function MenuApp() {
                       {
                       products.map(product =>
                         product.docdata.category === 'breakfast' ?
-                          (<Col xl={8} md={8} sm={12} xs={24}>
-                          <MenuCards
-                          selectedProduct={selectedProduct}
-                          product={product}
-                          id={product.id} 
-                          name={product.docdata.name} 
-                          price={product.docdata.price} 
-                          photo={product.docdata.photo}
-                          />
+                          (<Col xl={8} md={8} sm={12} xs={24} key={product.id} >
+                            <MenuCards handleAddProduct={handleAddProduct} product={product} />
                           </Col>) : false
                       )
                       }
@@ -129,15 +115,8 @@ function MenuApp() {
                       {
                       products.map(product =>
                         product.docdata.category !== 'breakfast' && product.docdata.category !== 'additional'?
-                        (<Col xl={8} md={8} sm={12} xs={24}>
-                        <MenuCards
-                        selectedProduct={selectedProduct}
-                        product={product}
-                        id={product.id} 
-                        name={product.docdata.name} 
-                        price={product.docdata.price} 
-                        photo={product.docdata.photo}
-                        />
+                        (<Col xl={8} md={8} sm={12} xs={24} key={product.id} >
+                          <MenuCards  handleAddProduct={handleAddProduct} product={product} />
                         </Col>) : false
                       )
                       }
@@ -152,15 +131,8 @@ function MenuApp() {
                       {
                       products.map(product =>
                         product.docdata.category === 'additional'?
-                          (<Col xl={8} md={8} sm={12} xs={24}>
-                          <MenuCards
-                          selectedProduct={selectedProduct}
-                          product={product}
-                          id={product.id} 
-                          name={product.docdata.name} 
-                          price={product.docdata.price}  
-                          photo={product.docdata.photo}
-                          />
+                          (<Col xl={8} md={8} sm={12} xs={24} key={product.id} >
+                            <MenuCards handleAddProduct={handleAddProduct} product={product} />
                           </Col>) : false
                       )
                       }
@@ -170,8 +142,14 @@ function MenuApp() {
                 </TabPane>
               </Tabs>
             </Col>
-            <Col xl={9} md={24} >
-                <OrderForm selectedProductsArray={selectedProductsArray} />
+            <Col xl={9} md={24} sm={22} xs={24}>
+                <OrderForm 
+                handleDeleteProduct={handleDeleteProduct}
+                handleMinusProduct={handleMinusProduct}
+                handlePlusProduct={handlePlusProduct}
+                selectedProductsArray={selectedProductsArray} 
+                setSelectedProductsArray={setSelectedProductsArray}
+                />
             </Col>
           </Row>
         </Content>
