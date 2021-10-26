0605c504731fe366c8d44b9415db5d600a01d4ff
margaZM <aurazambrano227@gmail.com>
2021-10-26 09:13:12 -0500

    FIX: Add return to login function

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
 import '../css/orderForm.css'
+import { serverTimestamp } from 'firebase/firestore';
+import { querySnapshot, addCollection } from '../firebase/firestore';
+import { db } from '../firebase/firebaseConfig'
 import {
   Form,
   Input,
@@ -16,62 +19,58 @@ import {
   MinusCircleOutlined,
   DeleteOutlined
 } from '@ant-design/icons';
-import { FieldValue, Timestamp, serverTimestamp } from 'firebase/firestore';
-import { querySnapshot } from '../firebase/firestore';
-import { db } from '../firebase/firebaseConfig'
 
-function OrderForm(props) {
-  // const objProducts = [];
-
-  // console.log(selectedProductsArray)
-  // const nuevaprueba = selectedProductsArray.filter(product => product.id)
-  // // const nameProduct = selectedProductsArray.reduce((acc, current) => (acc[current.name] ? acc[current.name]  += 1 : acc[current.name] = 1, acc), [])
-  // //Revisa si un producto ya está en el array
-  // const exist = selectedProductsArray.some(product => product.id === selectedProductsArray.id)
+const OrderForm = ({handleDeleteProduct, handleMinusProduct, handlePlusProduct, ...props}) => {
 
   const [form] = Form.useForm();
   const { Option } = Select;
+  const [numberOrder, setNumberOrder] = useState(1); //Agrega contador de numero de orden
+  const [statusOrder, setStatusOrder] = useState('preparing'); //Agrega status de la orden
+  const [tables, setTables] = useState([]); // Traer mesas disponibles
 
-  //------------------------ Traer ordenes de firestore -------------------------//
-  // const [order, setOrder] = useState([]);
+  //----------------------------- Traer mesas disponibles -------------------------------//
+  const getTables = async () => {
+    const data = await querySnapshot(db, "tables");
+    return data.docs.map((doc) => ({id: doc.id, docdata : doc.data()}))
+  };
 
-  // const getOrder = async () => {
-  //   // const data = await querySnapshot(db, "orders");
-  //   // return data.docs.map((doc) => ({id: doc.id, docdata : doc.data()}))
-  // };
+  useEffect(() => {
+    getTables().then((orderArray) => setTables(orderArray))
+  }, []);
 
-  // useEffect(() => {
-  //   getOrder().then((orderArray) => setOrders(orderArray))
-  // }, []);
-
-  //------------------------ Traer mesas disponibles -------------------------//
-
-
-
-  //------------------------ Enviar una orden a firestore -------------------------//
+  //---------------------------- Enviar una orden a firestore ----------------------------//
   const selectedProductsArray = props.selectedProductsArray;
-  const selectedProductsID = () => selectedProductsArray.map(product => product.id); //Array con los ID de los productos
-  const total = () => selectedProductsArray.reduce((acc, current )=> acc + current.total, 0) //Total de la cuenta
+  const selectedProductsID = () => selectedProductsArray.map(product => ({
+    name: product.name,
+    price: product.price,
+    id: product.id, 
+    quantity: product.quantity,
+    type: product.type === undefined ? "" : product.type,
+    }));
+  const total = () => selectedProductsArray.reduce((acc, current )=> acc + (current.price * current.quantity), 0) //Total de la cuenta
   const  subTotal = Math.round(total() / 1.18).toFixed(2);
   const tax = Math.round(subTotal* 0.18).toFixed(2);
 
-  //Leer los valores del Form 
+  // //--------------------------- Leer los valores del Formulario  ---------------------------//
   const onFinish = (values) => {
-    const newOrderObject = { //Determinar el estatus
+    const newOrderObject = {
+      numberOrder: numberOrder,
       client: values.clientname,
       order: selectedProductsID(),
-      other: values.message,
+      other: values.message === undefined ? "" :  values.message,
+      status: statusOrder,
       table: values.table,
-      time: Timestamp,
+      subtotal: subTotal,
+      tax: tax,
+      total: total(),
+      time: serverTimestamp(), // Date.now()  firebase.database.ServerValue.TIMESTAMP firebase.firestore.Timestamp.fromDate(new Date()); firebase.firestore.FieldValue.serverTimestamp()
     }
+    addCollection(db, 'orders', newOrderObject)//falta funcionalidad
+    setNumberOrder(numberOrder + 1) //OJO Necesito que este valor permanezca siempre actualizado
     console.log('Received values of form: ', newOrderObject, values);
+    form.resetFields()
+    props.setSelectedProductsArray([])
   };
-
-  //------------------------ Restar un producto del DOM -------------------------//
-  const minusProduct = (product) => {
-    console.log(product)
-  }
-
 
   return (
     <div className="site-card-border-less-wrapper order-list">
@@ -85,9 +84,10 @@ function OrderForm(props) {
             onFinish={onFinish}
             scrollToFirstError
             >
+          <p style={{textAlign: 'left', color: 'white'}}> Número de orden: 0000{numberOrder} </p> 
           <Form.Item
             style={{ width: 180}}
-            name="table" // label="Cargo"
+            name="table" 
             rules={[
             {
                 required: true,
@@ -96,15 +96,12 @@ function OrderForm(props) {
             ]}
             >
             <Select placeholder="Mesas disponibles...">
-              <Option value="mesa1">Mesa No. 01</Option>
-              <Option value="mesa2">Mesa No. 02</Option>
-              <Option value="mesa3">Mesa No. 03</Option>
-              <Option value="mesa4">Mesa No. 04</Option>
-              <Option value="mesa5">Mesa No. 05</Option>
-              <Option value="mesa6">Mesa No. 06</Option>
-              <Option value="mesa7">Mesa No. 07</Option>
-              <Option value="mesa8">Mesa No. 08</Option>
-              <Option value="mesa9">Mesa No. 09</Option>
+              {
+                tables.map((table) => (
+                  <Option key={table.id} className='option-select' value={table.id} disabled={table.docdata.status ? false: true}>{table.docdata.table} </Option>
+                ))
+              }
+
             </Select>
           </Form.Item>
 
@@ -127,13 +124,15 @@ function OrderForm(props) {
           {
             selectedProductsArray.map(product => {
               return (
-                <Row style={{ color: 'white' }}>
-                  <Col xl={12} md={12} sm={12} xs={12} className="product-name"> {product.name} </Col>
-                  <Col xl={2} md={2} sm={2} xs={2} > <MinusCircleOutlined  className="product-icon" style={{ fontSize: '110%'}} onClick={() => minusProduct(product)} /> </Col>
-                  <Col  xl={3} md={3} sm={3} xs={3}> {product.quantity} </Col>
-                  <Col xl={2} md={2} sm={2} xs={2}> <PlusCircleOutlined className="product-icon" style={{ fontSize: '110%'}} /> </Col>
-                  <Col  xl={3} md={3} sm={3} xs={3}> {'s/' + product.total} </Col>
-                  <Col xl={2} md={2} sm={2} xs={2}> <DeleteOutlined className="product-icon-delete" style={{ cursor: 'pointer', fontSize: '120%'}} /> </Col>
+                <Row style={{ color: 'white' }}
+                key={product.id}
+                >
+                  <Col xl={12} md={12} sm={12} xs={12} className="product-name"> {product.name} { product.type? product.type : null}</Col>
+                  <Col xl={2} md={2} sm={2} xs={2}  >  <MinusCircleOutlined   className="product-icon" style={{ fontSize: '110%'}} onClick={() => handleMinusProduct(product)} /> </Col>
+                  <Col  xl={3} md={3} sm={3} xs={3} > {product.quantity} </Col>
+                  <Col xl={2} md={2} sm={2} xs={2}  > <PlusCircleOutlined  className="product-icon" style={{ fontSize: '110%'}}  onClick={() => handlePlusProduct(product)} /> </Col>
+                  <Col  xl={3} md={3} sm={3} xs={3}  > {'s/' + product.quantity * product.price} </Col>
+                  <Col xl={2} md={2} sm={2} xs={2}  > <DeleteOutlined className="product-icon-delete" style={{ cursor: 'pointer', fontSize: '120%'}} onClick={() => handleDeleteProduct(product)}  /> </Col>
                 </Row>
               )
             }
@@ -142,7 +141,7 @@ function OrderForm(props) {
           </div> 
 
           <Form.Item name="message" label="Nota especial para el chef:" style={{ margin: '50px'}}>
-            <Input.TextArea showCount maxLength={100} />
+            <Input.TextArea showCount maxLength={100}/>
           </Form.Item>
 
           <Divider style={{ background: "white" }} />
@@ -161,11 +160,15 @@ function OrderForm(props) {
             <p className="title"> Total </p>
             <p className="title"> {'s/' + total().toFixed(2)} </p>
           </div>
-
+     
           <Form.Item >
-              <Button htmlType="submit" className="form-button">
+            <Button ghost htmlType="reset" style={{ width: "45%", marginRight: "10%", height: 50, borderRadius: 15, fontWeight: 'bold', fontSize: 16}}>
+              CANCELAR
+            </Button>
+
+            <Button htmlType="submit" className="form-button" style={{ width: "45%" }}>
               CREAR ORDEN
-              </Button>
+            </Button>
           </Form.Item>
         </Form>  
       </Card>
