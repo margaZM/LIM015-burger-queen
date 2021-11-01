8683392d2ad72468e69b4131f8bd5a6f9c1f220d
Andrea Blanco <estefania_8_3@hotmail.com>
2021-11-01 18:59:52 -0500

    test: add testing structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
-// import App from './App';
 import Error404 from './pages/Error404';
-import Logo from './pages/Delivered';
-import ProductList from './pages/Delivered';
+import Logo from './components/Logo.jsx';
+import ProductList from './components/ProductsList.jsx';
 import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -11,7 +9,23 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('<Error404 />', () => {
   it('renders three <Error404 /> components', () => {
     const wrapper = shallow(<Error404 />);
+    expect(wrapper.exists()).toBeTruthy()
+  });
+  it('renders paragraph oops', () => {
+    const wrapper = shallow(<Error404 />);
     expect(wrapper.contains(<p className="text-error">Oops! Lo sentimos pero no se encontro lo que buscabas</p>)).toEqual(true);
+  });
+  it('renders paragraph about options', () => {
+    const wrapper = shallow(<Error404 />);
+    expect(wrapper.contains(<p className="text-error">O tal vez quisiste visitar alguno de los sgtes links:</p>)).toEqual(true);
+  });
+  it('renders ancle element with route to home', () => {
+    const wrapper = shallow(<Error404 />);
+    expect(wrapper.contains(<a href="/">Inicio</a>)).toEqual(true);
+  });
+  it('renders ancle element with route to register', () => {
+    const wrapper = shallow(<Error404 />);
+    expect(wrapper.contains(<a href="/register">Registro</a>)).toEqual(true);
   });
 });
 
@@ -19,6 +33,13 @@ describe('<Logo />', () => {
   it('renders three <Logo /> components', () => {
     const wrapper = shallow(<Logo />);
     expect(wrapper.exists()).toBeTruthy()
+  });
+  it('renders paragraph about options', () => {
+    const wrapper = shallow(<div>
+            <img className="logo" src="./images/logo.svg" alt="logo-burger-queen"/>
+        </div>);
+    // const input = wrapper.find('img');
+    expect(wrapper.exists('img')).toBe(true);
   });
 });
 
